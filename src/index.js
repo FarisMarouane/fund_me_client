e38249de481ae36419d1cc108149fe6d49b21eba
Marouane Faris <faris.marouane@gmail.com>
2022-03-21 12:02:26 +0100

    Add web3-react lib

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Web3ReactProvider } from '@web3-react/core';
+import { ethers } from 'ethers';
+
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+function getLibrary(provider) {
+  return new ethers.providers.Web3Provider(window.ethereum);
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Web3ReactProvider getLibrary={getLibrary}>
+      <App />
+    </Web3ReactProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById('root'),
 );
 
 // If you want to start measuring performance in your app, pass a function
